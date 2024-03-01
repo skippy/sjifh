c54cc9dcad0fd34f701a44fda467ba826cbf30fb
Adam Greene <adam.greene@gmail.com>
2024-02-29 18:13:06 -0800

    Update producer_payments.js
    make sure we're correctly calculating the producer payment when the same producer shows up in multiple sub-periods (like regular and wholesale)

--- bin/producer_payments.js
+++ bin/producer_payments.js
@@ -30,6 +30,10 @@ const argv = require('yargs/yargs')(hideBin(process.argv))
     type: 'boolean',
     default: false
   })
+  .option('periodDate', {
+    alias: 'p',
+    description: 'which period date, as displayed, to run'
+  })
   .count('verbose')
   .alias('v', 'verbose')
   .demandOption([])
@@ -48,10 +52,10 @@ switch (argv.verbose) {
     logger.level = 'debug'
 }
 
-async function processProducerFee (name, subperiod, amtPayable, page) {
+async function processProducerFee (name, subperiod, amtPayable, location, page) {
   let fees = null
   let feeNote = null
-  logger.debug(`processProducerFee: ${name} -- ${subperiod} -- ${amtPayable} -- ${NON_MEMBERS.indexOf(name)}`)
+  logger.debug(`processProducerFee: ${name} -- ${subperiod} -- ${amtPayable} -- location: ${location} -- non-member? ${NON_MEMBERS.indexOf(name) != -1}`)
   if (NON_MEMBERS.indexOf(name) != -1) {
     fees = -1 * amtPayable * NON_MEMBER_FEE
     feeNote = NON_MEMBER_NOTE
@@ -61,28 +65,14 @@ async function processProducerFee (name, subperiod, amtPayable, page) {
   }
   logger.verbose(` ${name} (${subperiod}) \$${fees} - "${feeNote}"`)
   logger.debug('    opening modal')
-  page.click(`table.sticky-table-header tr td:text("${name}")`)
+  page.locator(`table.sticky-table-header tr`).nth(location+1).locator(`td:text("${name}")`).click()
   await page.fill('#paytoAmount', fees.toString())
   if (fees !== 0) {
     await page.fill('#paytoNote', feeNote)
   }
+  // await page.screenshot({ path: 'output/producer_payment_model_' + name + '_' + subperiod + '-2.png' });
   await page.click('button:text("Update")')
-
-  // await page.click('a span.uk-icon-plus-circle');
-  // await page.selectOption('#paytoProducer', {label: name});
-  // await page.selectOption('#paytoSubPeriod', {label: subperiod});
-  // await page.fill('#paytoAmount', fees.toString())
-  // await page.fill('#paytoNote', feeNote)
-  // the button is clicked by lets wait until the model is closed as the
-  // underlying network event may not have been triggered; wait for
-  // the modal to close.
-  // await page.screenshot({ path: 'output/producer_payment_model_' + name + '-1.png' });
-  // await page.click('button:text("Create Payment")')
-  // if already exists, lets close and put a warning
-  // if uk-notify-message uk-notify-message-warning
-  //   uk-modal-close uk-close
   await page.waitForSelector('#producerPaymentModal', { state: 'hidden' })
-  // await page.screenshot({ path: 'output/producer_payment_model_' + name + '-2.png' });
   logger.debug('    submitting modal')
 }
 
@@ -101,70 +91,45 @@ async function processProducerFee (name, subperiod, amtPayable, page) {
 
   await page.goto(PRODUCER_PAYMENT_URL)
   await page.click('#periodId')
-  // wait until we have a new period running before calculating the old closed period
-  await page.locator('.select-checkbox-list .internal-periodId li:nth-child(3) input').click()
+  logger.debug(`periodDate: '${argv.periodDate}'`)
+  if (argv.periodDate) {
+    logger.debug(`selecting date: ${argv.periodDate}`)
+    await page.locator(`.select-checkbox-list li.select-checkbox-option :text("${argv.periodDate}")`).click()
+  }else{
+    // wait until we have a new period running before calculating the old closed period
+    await page.locator('.select-checkbox-list .internal-periodId li:nth-child(3) input').click()
+  }
 
   await page.click('button:text("Actions")')
   await page.click('a:text("Calculate Producer Payments")')
   await page.click('button:text("OK")')
 
   await page.waitForSelector('.uk-container .sticky-table-header', { state: 'visible' })
-  await page.screenshot({ path: 'output/producer_page1.png' })
+  // await page.screenshot({ path: 'output/producer_page1.png' })
 
-  // // Bulk update adjustment amount
-  // await page.click('#checkAllItems input')
-  // await page.click('button:text("Bulk Actions")')
-  // await page.click('a:text("Update Adjustment")')
-  // await page.fill('#bulkAdjustment', '-3')
-  // await page.selectOption('#bulkAdjustmentType', 'percent')
-  // await page.click('button:text("Add Adjustment")')
-
-  // // bulk update adjustment note
-  // await page.click('#checkAllItems input')
-  // await page.click('button:text("Bulk Actions")')
-  // await page.click('a:text("Update Note")')
-  // await page.fill('#bulkNote', MEMBER_NOTE)
-  // await page.click('button:text("Add Note")')
-
-  // for (let i = 0; i < SKIP_PRODUCERS.length; i++) {
-  //   let prodName = SKIP_PRODUCERS[i];
-  //   page.click("table.sticky-table-header tr td:text('" + prodName + "')")
-  //   await page.fill('#paytoAmount', '')
-  //   await page.fill('#paytoNote', '')
-  //   await page.click('button:text("Update")')
-  // }
-
-  // for (let i = 0; i < NON_MEMBERS.length; i++) {
-  //   let prodName = NON_MEMBERS[i];
-  //   page.click("table.sticky-table-header tr td:text('" + prodName + "')")
-  //   await page.fill('#paytoAmount', '')
-  //   await page.fill('#paytoNote', '')
-  //   await page.click('button:text("Update")')
-  // }
-
+  const periodDate = await page.locator('.select-checkbox-list .internal-periodId .select-checkbox-placeholder').allTextContents()
   const allProducerNames = await page.locator('table.sticky-table-header tr td:nth-child(1)').allTextContents()
   const allProducerSubPeriods = await page.locator('table.sticky-table-header tr td:nth-child(3)').allTextContents()
   const allProducerAmtsPayable = await page.locator('table.sticky-table-header tr td:nth-child(4)').allTextContents()
-  const producerNames = _.chain(allProducerNames).uniq().without(...SKIP_PRODUCERS).value()
+  const uniqProducerNames = _.chain(allProducerNames).uniq().without(...SKIP_PRODUCERS).value()
 
-  logger.debug('--------------')
+  logger.debug(periodDate)
   logger.debug(allProducerNames)
   logger.debug(allProducerSubPeriods)
   logger.debug(allProducerAmtsPayable)
-  logger.debug(producerNames)
-  logger.debug('--------------')
 
-  for (let i = 0; i < producerNames.length; i++) {
-    const pn = producerNames[i]
-    const locs = _.filter(_.range(allProducerNames.length), (i) => allProducerNames[i] === pn)
-    for (let j = 0; j < locs.length; j++) {
-      const loc = locs[j]
+  for (let loc = 0; loc < allProducerNames.length; loc++) {
+    const pn = allProducerNames[loc]
+    //ie, if producer is not on the skip list
+    // do this here rather than use producerNames so we can keep the location tied to
+    // the producer AND sub-period
+    if (SKIP_PRODUCERS.indexOf(pn) < 0){
       const amtPayable = parseFloat(allProducerAmtsPayable[loc].replace('$', '').replace(',', ''))
       const subperiod = allProducerSubPeriods[loc]
-      await processProducerFee(pn, subperiod, amtPayable, page)
+      await processProducerFee(pn, subperiod, amtPayable, loc, page)
     }
   }
 
   browser.close()
-  logger.info(`Finished adding fees to ${producerNames.length} producers`)
+  logger.info(`Finished adding fees to ${uniqProducerNames.length} producers`)
 })()
